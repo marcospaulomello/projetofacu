70eeaaaf155a964a4b093018a1b9524e2937ca51
Marcos Paulo <marcosmello.mp@gmail.com>
2019-03-21 20:10:19 -0300

    ok

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Calcular IMC'),
+      home: MyHomePage(title: 'Multiplicar valores'),
     );
   }
 }
@@ -46,13 +46,13 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
-  double _imc = 0.0;
-  double alt = 0.0;
-  double peso = 0.0;
+  double _mult = 0.0;
+  double valor1 = 0.0;
+  double valor2 = 0.0;
 
 
 
-  void _calcIMC(double a, double p) {
+  void _calcMULT(double a, double p) {
     print("First text field:");
     setState(() {
       // This call to setState tells the Flutter framework that something has
@@ -60,7 +60,7 @@ class _MyHomePageState extends State<MyHomePage> {
       // so that the display can reflect the updated values. If we changed
       // _counter without calling setState(), then the build method would not be
       // called again, and so nothing would appear to happen.
-      _imc = p /(a*a);
+      _mult = a*p;
     });
   }
 
@@ -101,7 +101,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
             new TextField(
                 onChanged: (text) {
-                  alt = double.parse("$text");
+                  valor1 = double.parse("$text");
                 },
                 decoration: InputDecoration(
                 border: InputBorder.none,
@@ -111,7 +111,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
             new TextField(
                 onChanged: (text) {
-                  peso = double.parse("$text");
+                  valor2 = double.parse("$text");
                 },
                 decoration: InputDecoration(
                 border: InputBorder.none,
@@ -120,21 +120,21 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
 
             new Text(
-              'Seu IMC é:',
+              'A multiplicação dos valores é:',
             ),
 
             new Text(
-              '$_imc',
+              '$_mult',
               style: Theme.of(context).textTheme.display1,
             ),
 
             new RaisedButton(
-                child: const Text ('Calcular'),
+                child: const Text ('Multiplicar'),
                 color: Theme.of(context).accentColor,
                 elevation: 4.0,
                 splashColor: Colors.blueGrey,
                 onPressed: () {
-                  _calcIMC(alt, peso);
+                  _calcMULT(valor1, valor2);
                 },
             ),
 
